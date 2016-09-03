f4391895a625227aa22432364e6b8c8e0e34a4c0
SwannSG <swannsg@gmail.com>
2016-09-03 18:49:48 +0200

    modified:   client/layout.css  	modified:   client/main.html  	modified:   imports/ui/settingsForm.jsx

--- imports/ui/settingsForm.jsx
+++ imports/ui/settingsForm.jsx
@@ -9,10 +9,10 @@ class SettingsForm extends Component {
         super(props);
         this.state = {
            showOppPointCount: this.props.settings.showOppPointCount,
-        //    showOppCardFace: this.props.settings.showOppCardFace,
-        //    showPartnersCardFace: this.props.settings.showPartnersCardFace,
-        //    showPartnersPointCount: this.props.settings.showPartnersPointCount,
-        //    showOwnPointCount: this.props.settings.showOwnPointCount,
+           showOppCardFace: this.props.settings.showOppCardFace,
+           showPartnersCardFace: this.props.settings.showPartnersCardFace,
+           showPartnersPointCount: this.props.settings.showPartnersPointCount,
+           showOwnPointCount: this.props.settings.showOwnPointCount,
            cycle: 'wip'
        };
        this.updateState = this.updateState.bind(this);
@@ -29,6 +29,7 @@ class SettingsForm extends Component {
         //     showPartnersPointCount: this.refs.showPartnersPointCount.checked,
         //     showOwnPointCount: this.refs.showOwnPointCount.checked,
         // });
+        console.log(this.refs.showOppPointCount.checked);
         this.setState({cycle:'exit'});
     }
 
@@ -41,7 +42,6 @@ class SettingsForm extends Component {
 
     render() {
         console.log('render');
-        console.log(this.props.settings.showOppPointCount);
         if (this.state.cycle==='exit') {
             // set null in DOM
             return null;
@@ -55,7 +55,23 @@ class SettingsForm extends Component {
                 <div className={styles.body}>
                     <div className={styles.labelAndField}>
                         <label className={styles.label}>Show opponents point count and distribution</label>
-                        <input className={styles.checkbox}type="checkbox" name="showOppPointCount" ref="showOppPointCount"/>
+                        <input className={styles.checkbox} type="checkbox" ref="showOppPointCount" name="showOppPointCount" checked={this.state.showOppPointCount} onChange={this.updateState}/>
+                    </div>
+                    <div className={styles.labelAndField}>
+                        <label className={styles.label}>Show opponents cards</label>
+                        <input className={styles.checkbox} type="checkbox" name="showOppCardFace" ref="showOppPointCount" checked={this.state.showOppCardFace} onChange={this.updateState}/>
+                    </div>
+                    <div className={styles.labelAndField}>
+                        <label className={styles.label}>Show partners cards</label>
+                        <input className={styles.checkbox} type="checkbox" name="showPartnersCardFace" ref="showPartnersCardFace" checked={this.state.showPartnersCardFace} onChange={this.updateState}/>
+                    </div>
+                    <div className={styles.labelAndField}>
+                        <label className={styles.label}>Show partners point count and distribution</label>
+                        <input className={styles.checkbox} type="checkbox" name="showPartnersPointCount" ref="showPartnersPointCount" checked={this.state.showPartnersPointCount} onChange={this.updateState}/>
+                    </div>
+                    <div className={styles.labelAndField}>
+                        <label className={styles.label}>Show own point count and distribution</label>
+                        <input className={styles.checkbox} type="checkbox" name="showOwnPointCount" ref="showOwnPointCount" checked={this.state.showOwnPointCount} onChange={this.updateState}/>
                     </div>
                 </div>
                 <div className={styles.footer}></div>
@@ -63,39 +79,3 @@ class SettingsForm extends Component {
         );
     }
 }
-
-// return (
-//     <div parent style={[styles.parent, dyn.parent]}>
-//         <div heading style={[styles.heading, dyn.heading]}>
-//             <h4 header style={[styles.header, dyn.header]}>Settings</h4>
-//             <button close style={[styles.close, dyn.close]} type="button" onClick={this.handleExit}>&times;</button>
-//         </div>
-//
-//
-//         <div  style={[dyn.formDiv]}>
-//             <form>
-//                 <label style={[styles.label, dyn.label]}>Show opponents point count and distribution
-//                 <input style={[styles.input, dyn.input]} type="checkbox" name="showOppPointCount" ref="showOppPointCount"
-//                  checked={this.state.showPointCount} onChange={this.updateState} /></label>
-//
-//                 <label style={[styles.label, dyn.label]}>Show opponents cards
-//                 <input style={[styles.input, dyn.input]} type="checkbox" name="showOppCardFace" ref="showOppCardFace"
-//                 checked={this.state.showOppCardFace} onChange={this.updateState} /></label>
-//
-//                 <label style={[styles.label, dyn.label]}>Show partners cards
-//                 <input style={[styles.input, dyn.input]} type="checkbox" name="showPartnersCardFace" ref="showPartnersCardFace"
-//                  checked={this.state.showPartnersCardFace} onChange={this.updateState} /></label>
-//
-//                  <label style={[styles.label, dyn.label]}>Show partners point count and distribution
-//                  <input style={[styles.input, dyn.input]}type="checkbox" name="showPartnersPointCount" ref="showPartnersPointCount"
-//                   checked={this.state.showPartnersCount} onChange={this.updateState} /></label>
-//
-//                 <label style={[styles.label, dyn.label]}>Show own point count and distribution
-//                 <input style={[styles.input, dyn.input]} type="checkbox" name="showOwnPointCount" ref="showOwnPointCount"
-//                 checked={this.state.showOwnPointCount} onChange={this.updateState} /></label>
-//             </form>
-//         </div>
-//         <div footer="1" style={[dyn.footer]}></div>
-//     </div>
-// );
-//
